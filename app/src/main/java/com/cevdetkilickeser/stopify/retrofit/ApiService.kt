56cdc310fd29896737076b2e78b361d84d01ac1b
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-07-29 09:46:39 +0300

    retrofit added

---
+++ app/src/main/java/com/cevdetkilickeser/stopify/retrofit/ApiService.kt
@@ -0,0 +1,7 @@
+package com.cevdetkilickeser.stopify.retrofit
+
+import retrofit2.http.GET
+
+interface ApiService {
+
+}
---
+++ app/src/main/java/com/cevdetkilickeser/stopify/retrofit/ApiUtils.kt
@@ -0,0 +1,11 @@
+package com.cevdetkilickeser.stopify.retrofit
+
+class ApiUtils {
+    companion object {
+        val BASE_URL = "https://api.deezer.com/"
+
+        fun getApiService(): ApiService {
+            return RetrofitClient.getClient(BASE_URL).create(ApiService::class.java)
+        }
+    }
+}
