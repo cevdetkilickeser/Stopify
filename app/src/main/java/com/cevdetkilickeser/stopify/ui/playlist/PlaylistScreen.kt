c2c09e64facea84c38638f1dc2b3bb2324ff67df
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-08-02 15:28:27 +0300

    isSearch parameter added to PlaylistScreen and SearchScreen to show or make visible like button

--- app/src/main/java/com/cevdetkilickeser/stopify/ui/playlist/PlaylistScreen.kt
+++ app/src/main/java/com/cevdetkilickeser/stopify/ui/playlist/PlaylistScreen.kt
@@ -4,15 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.cevdetkilickeser.stopify.data.entity.Like
-import com.cevdetkilickeser.stopify.ui.search.TrackList
+import com.cevdetkilickeser.stopify.ui.component.TrackList
 import com.cevdetkilickeser.stopify.viewmodel.VMPlaylist
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun PlaylistScreen(playlistId: String, viewModel: VMPlaylist = hiltViewModel()) {
+fun PlaylistScreen(navController: NavController, playlistId: String, viewModel: VMPlaylist = hiltViewModel()) {
     val userId = FirebaseAuth.getInstance().currentUser!!.toString()
     LaunchedEffect(key1 = playlistId, key2 = userId) {
         viewModel.getPlaylistDataList(playlistId)
@@ -21,7 +21,14 @@ fun PlaylistScreen(playlistId: String, viewModel: VMPlaylist = hiltViewModel()) 
 
     val trackList by viewModel.trackListState.collectAsState()
     val likeList by viewModel.likeListState.collectAsState()
-    TrackList(trackList = trackList, likeList = likeList, onTrackClick = {}, onLikeClick = { track, isLike ->
+    TrackList(
+        isSearch = false,
+        trackList = trackList,
+        likeList = likeList,
+        onTrackClick = { track ->
+            //navigate to MusicPlayerScreen
+        },
+        onLikeClick = { track, isLike ->
         if (isLike){
             viewModel.deleteLikeByTrackId(userId, track.id)
         } else {
