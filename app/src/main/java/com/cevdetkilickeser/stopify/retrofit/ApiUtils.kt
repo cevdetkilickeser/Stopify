c44d8241873dc782d6317b5893ea4ecd38079a32
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-08-09 00:31:47 +0300

    Splash Api  added

--- app/src/main/java/com/cevdetkilickeser/stopify/retrofit/ApiUtils.kt
+++ app/src/main/java/com/cevdetkilickeser/stopify/retrofit/ApiUtils.kt
@@ -2,7 +2,7 @@ package com.cevdetkilickeser.stopify.retrofit
 
 class ApiUtils {
     companion object {
-        val BASE_URL = "https://api.deezer.com/"
+        private const val BASE_URL = "https://api.deezer.com/"
 
         fun getApiService(): ApiService {
             return RetrofitClient.getClient(BASE_URL).create(ApiService::class.java)
