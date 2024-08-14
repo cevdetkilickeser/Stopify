9fcdcf5168dda864246e7abb368edbd908efaf4f
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-08-14 03:20:18 +0300

    NetworkMonitor converted to singleton for Hilt

--- app/src/main/java/com/cevdetkilickeser/stopify/NetworkMonitor.kt
+++ app/src/main/java/com/cevdetkilickeser/stopify/NetworkMonitor.kt
@@ -5,33 +5,38 @@ import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class NetworkMonitor(context: Context) {
+@Singleton
+class NetworkMonitor @Inject constructor(@ApplicationContext private val context: Context) {
 
     private val connectivityManager =
         context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
-    private var networkCallback: ConnectivityManager.NetworkCallback? = null
-
-    fun startNetworkCallback(onNetworkAvailable: () -> Unit, onNetworkLost: () -> Unit) {
-        networkCallback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                onNetworkAvailable()
-            }
-
-            override fun onLost(network: Network) {
-                onNetworkLost()
-            }
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            super.onAvailable(network)
+            onNetworkStatusChanged(true)
         }
+
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            onNetworkStatusChanged(false)
+        }
+    }
+
+    fun startNetworkCallback() {
         val networkRequest = NetworkRequest.Builder()
             .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
             .build()
-        connectivityManager.registerNetworkCallback(networkRequest, networkCallback!!)
+        connectivityManager.registerNetworkCallback(networkRequest, networkCallback)
     }
 
     fun stopNetworkCallback() {
-        networkCallback?.let {
-            connectivityManager.unregisterNetworkCallback(it)
-        }
+        connectivityManager.unregisterNetworkCallback(networkCallback)
     }
+
+    var onNetworkStatusChanged: (Boolean) -> Unit = {}
 }
