a6238b7247daea9e97ee56b95c83a5ee3d594f76
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-08-14 03:18:55 +0300

    OfflineInfo composable added for if no internet connection

---
+++ app/src/main/java/com/cevdetkilickeser/stopify/ui/component/OfflineInfo.kt
@@ -0,0 +1,38 @@
+package com.cevdetkilickeser.stopify.ui.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OfflineInfo(
+    onClick: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Offline Mode!",
+            color = Color.Red
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(
+            text = "Go Downloads",
+            color = Color.Blue,
+            modifier = Modifier
+                .clickable { onClick() }
+        )
+    }
+}
