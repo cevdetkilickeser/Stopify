c4b3f3b58415f48f0f9af2d3c4d7df4980cd2669
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-08-15 10:38:45 +0300

    userId parameter added to Download

--- app/src/main/java/com/cevdetkilickeser/stopify/data/entity/Download.kt
+++ app/src/main/java/com/cevdetkilickeser/stopify/data/entity/Download.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "download_table")
 data class Download(
     @PrimaryKey(autoGenerate = true) val downloadId: Long = 0,
+    @ColumnInfo(name = "userId") val userId: String,
     @ColumnInfo(name = "fileUri") val fileUri: String? = null,
     @ColumnInfo(name = "trackId") val trackId: String,
     @ColumnInfo(name = "trackPreview") val trackPreview: String,
