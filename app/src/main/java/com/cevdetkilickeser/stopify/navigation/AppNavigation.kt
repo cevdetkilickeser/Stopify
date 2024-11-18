9329438efad53e26b21cccb7d2197750b8b4217c
Cevdet Kilickeser <kilickesercevdet@gmail.com>
2024-11-18 12:37:38 +0300

    imports cleaned in AppNAvigation

--- app/src/main/java/com/cevdetkilickeser/stopify/navigation/AppNavigation.kt
+++ app/src/main/java/com/cevdetkilickeser/stopify/navigation/AppNavigation.kt
@@ -1,36 +1,13 @@
 package com.cevdetkilickeser.stopify.navigation
 
 import android.app.Activity
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.cevdetkilickeser.stopify.data.model.player.PlayerTrack
-import com.cevdetkilickeser.stopify.json
-import com.cevdetkilickeser.stopify.ui.album.AlbumScreen
-import com.cevdetkilickeser.stopify.ui.artist.ArtistScreen
-import com.cevdetkilickeser.stopify.ui.downloads.DownloadsScreen
-import com.cevdetkilickeser.stopify.ui.home.HomeScreen
-import com.cevdetkilickeser.stopify.ui.likes.LikesScreen
-import com.cevdetkilickeser.stopify.ui.login.LoginScreen
-import com.cevdetkilickeser.stopify.ui.musicplayer.MusicPlayerScreen
-import com.cevdetkilickeser.stopify.ui.playlist.PlaylistScreen
-import com.cevdetkilickeser.stopify.ui.profile.ProfileScreen
-import com.cevdetkilickeser.stopify.ui.search.SearchScreen
-import com.cevdetkilickeser.stopify.ui.signup.SignupScreen
-import com.cevdetkilickeser.stopify.ui.singlegenre.SingleGenreScreen
-import com.cevdetkilickeser.stopify.ui.userplaylist.UserPlayListScreen
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
